f9c3f4761acf3c80c759bc9f6b7301cbcd512e73
Nacho Betancourt <trsgarp@gmail.com>
2023-07-18 09:49:31 -0600

    fix more indents

--- test/dxf/parser.rb
+++ test/dxf/parser.rb
@@ -2,59 +2,59 @@ require 'minitest/autorun'
 require 'dxf/parser'
 
 describe DXF::Parser do
-    it 'must read from an IO stream' do
-	File.open('test/fixtures/circle.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-    end
+	it 'must read from an IO stream' do
+		File.open('test/fixtures/circle.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+	end
 
-    it 'must parse a file with a circle' do
-	parser = File.open('test/fixtures/circle.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-	parser.entities.length.must_equal 1
-	circle = parser.entities.last
-	circle.must_be_instance_of(DXF::Circle)
-	circle.center.must_equal Geometry::Point[0,0]
-	circle.radius.must_equal 1
-    end
+	it 'must parse a file with a circle' do
+		parser = File.open('test/fixtures/circle.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+		parser.entities.length.must_equal 1
+		circle = parser.entities.last
+		circle.must_be_instance_of(DXF::Circle)
+		circle.center.must_equal Geometry::Point[0,0]
+		circle.radius.must_equal 1
+	end
 
-    it 'must parse a file with an arc' do
-	parser = File.open('test/fixtures/arc.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-	parser.entities.length.must_equal 1
-	arc = parser.entities.last
-	arc.must_be_instance_of DXF::Arc
+	it 'must parse a file with an arc' do
+		parser = File.open('test/fixtures/arc.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+		parser.entities.length.must_equal 1
+		arc = parser.entities.last
+		arc.must_be_instance_of DXF::Arc
 
-	arc.center.must_equal Geometry::Point[1,1]
-	arc.radius.must_equal 1
-	arc.start_angle.must_equal 180.0
-	arc.end_angle.must_equal 270.0
-    end
+		arc.center.must_equal Geometry::Point[1,1]
+		arc.radius.must_equal 1
+		arc.start_angle.must_equal 180.0
+		arc.end_angle.must_equal 270.0
+	end
 
-    it 'must parse a file with a translated circle' do
-	parser = File.open('test/fixtures/circle_translate.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-	parser.entities.length.must_equal 1
-	circle = parser.entities.last
-	circle.must_be_instance_of(DXF::Circle)
-	circle.center.must_equal Geometry::Point[1,1]
-	circle.radius.must_equal 1
-    end
+	it 'must parse a file with a translated circle' do
+		parser = File.open('test/fixtures/circle_translate.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+		parser.entities.length.must_equal 1
+		circle = parser.entities.last
+		circle.must_be_instance_of(DXF::Circle)
+		circle.center.must_equal Geometry::Point[1,1]
+		circle.radius.must_equal 1
+	end
 
-    it 'must parse a file with a lightweight polyline' do
-	parser = File.open('test/fixtures/square_lwpolyline_inches.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-	parser.entities.length.must_equal 1
-	parser.entities.all? {|a| a.kind_of? DXF::LWPolyline }.must_equal true
-	parser.entities.first.points.length.must_equal 4
-    end
+	it 'must parse a file with a lightweight polyline' do
+		parser = File.open('test/fixtures/square_lwpolyline_inches.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+		parser.entities.length.must_equal 1
+		parser.entities.all? {|a| a.kind_of? DXF::LWPolyline }.must_equal true
+		parser.entities.first.points.length.must_equal 4
+	end
 
-    it 'must parse a file with a square' do
-	parser = File.open('test/fixtures/square_inches.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-	parser.entities.length.must_equal 4
-	line = parser.entities.last
-	line.must_be_instance_of(DXF::Line)
-	line.first.must_equal Geometry::Point[0, 1]
-	line.last.must_equal Geometry::Point[0, 0]
-    end
+	it 'must parse a file with a square' do
+		parser = File.open('test/fixtures/square_inches.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+		parser.entities.length.must_equal 4
+		line = parser.entities.last
+		line.must_be_instance_of(DXF::Line)
+		line.first.must_equal Geometry::Point[0, 1]
+		line.last.must_equal Geometry::Point[0, 0]
+	end
 
-    it 'must parse a file with a spline' do
-	parser = File.open('test/fixtures/spline.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-	parser.entities.length.must_equal 82
-	parser.entities.all? {|a| a.kind_of? DXF::Spline }.must_equal true
-    end
+	it 'must parse a file with a spline' do
+		parser = File.open('test/fixtures/spline.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+		parser.entities.length.must_equal 82
+		parser.entities.all? {|a| a.kind_of? DXF::Spline }.must_equal true
+	end
 end
--- test/dxf/test.rb
+++ test/dxf/test.rb
@@ -2,10 +2,10 @@ require 'dxf/parser'
 require 'byebug'
 
 describe DXF::Parser do
-    it 'must read from an IO stream' do
-	    File.open('test/fixtures/test.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-    end
-    it 'must read from an IO stream with Polyline' do
-	    File.open('test/fixtures/fix.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
-    end
+  it 'must read from an IO stream' do
+	  File.open('test/fixtures/test.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+  end
+  it 'must read from an IO stream with Polyline' do
+	  File.open('test/fixtures/fix.dxf', 'r') {|f| DXF::Parser.new.parse(f) }
+  end
 end
